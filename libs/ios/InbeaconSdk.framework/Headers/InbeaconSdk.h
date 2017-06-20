c48d4e3f9cf00ad5f080e9723d29b6ea284fa1c0
Ronald van Woensel <rvw@cosninix.com>
2017-06-20 13:47:43 +0200

    incremental

---
+++ libs/ios/InbeaconSdk.framework/Headers/InbeaconSdk.h
@@ -0,0 +1,25 @@
+//
+//  inBeaconSdk.h
+//  inBeaconSdk
+//
+//  Created by rvw on 22-3-2016.
+//  Copyright © 2016 inBeacon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for inBeaconSdk.
+FOUNDATION_EXPORT double InbeaconSdkVersionNumber;
+
+//! Project version string for inBeaconSdk.
+FOUNDATION_EXPORT const unsigned char InbeaconSdkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <inBeaconSdk/PublicHeader.h>
+// #import <InbeaconSdk/objcClass.h>
+#import <InbeaconSdk/Hmac.h>
+#import <InbeaconSdk/WebViewJavascriptBridge.h>
+#import <InbeaconSdk/KLCPopup.h>
+
+
+
+
